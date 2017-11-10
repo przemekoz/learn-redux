f135ac0cacd9d9f22bec65442aed14c42d131813
Przemek Kozinski <przemyslaw.kozinski@alan-systems.com>
2017-11-10 14:48:14 +0100

    nxt changes

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
+
+// Import css
 import css from './styles/style.styl';
 
+// Import components
 import Main from './components/Main';
 
 render(<Main />, document.getElementById('root'));
